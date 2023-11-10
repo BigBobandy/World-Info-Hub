e2bd830a35cabf3f88c9427fd7c06cd7eb05650d
BigBobandy <jdykes207@gmail.com>
2023-11-10 12:53:43 -0500

    Update countryService.js
    with the correct request url for production

--- client/build/services/countryService.js
+++ client/build/services/countryService.js
@@ -1,12 +1,11 @@
 export const fetchCountries = async () => {
-    try {
-        const response = await fetch("http://localhost:3000/api/countries");
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        return await response.json();
+  try {
+    const response = await fetch("http://localhost:7338/api/countries");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
-    catch (error) {
-        console.error("There has been a problem with your fetch operation:", error);
-    }
+    return await response.json();
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
 };
